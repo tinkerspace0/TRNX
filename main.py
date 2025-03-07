2f4f46b5b262c79a01c2b985f365be945a1fe3ce
George Gil <136652865+georgegil3902@users.noreply.github.com>
2025-03-07 17:26:04 +0100

    A simple working bot configured

--- main.py
+++ main.py
@@ -5,25 +5,39 @@ from trenex import Trenex
 todo = 1
 
 if todo == 1:
+
+    from core.plugin import package_plugin
+
+    package_plugin("plugins/Binance")
+    package_plugin("plugins/RSI")
+    package_plugin("plugins/Agent")
+
+
     trenex = Trenex()
     trenex.start_new_trnx("trnx1")
     trenex.load_plugin("plugins/Binance.zip")
     trenex.load_plugin("plugins/RSI.zip")
+    trenex.load_plugin("plugins/Agent.zip")
 
-    trenex.define_plugin_output_to_input("Binance", "OHLCV", "Rsi", "ohlcv")
+    trenex.define_plugin_output_to_input("Binance", "ohlcv", "Rsi", "ohlcv")
+    trenex.define_plugin_output_to_input("Rsi", "rsi", "Agent", "rsi")
 
     trenex.build_trnx()
     trenex.get_trnx().run()
 
 
-else:
-# from core.plugin import create_template 
-
-# create_template("RSI", "dataprocessor", "plugins")
-
+elif todo == 2:
 
 
     from core.plugin import package_plugin
 
     package_plugin("plugins/Binance")
     package_plugin("plugins/RSI")
+    package_plugin("plugins/Agent")
+
+
+# else:
+#     from core.plugin import create_template 
+
+#     create_template("Agent", "dataprocessor", "plugins")
+
