b632687e86c060e829047de876f87e4ef6f5b2f2
George Gil <136652865+georgegil3902@users.noreply.github.com>
2025-04-19 20:34:36 +0200

    removed api for now

--- api/fastapi/config.py
+++
@@ -1,3 +0,0 @@
-# api/fastapi/config.py
-# You can add FastAPI-specific configuration here.
-FASTAPI_SETTING = "default_value"
--- api/fastapi/dependencies.py
+++
@@ -1,8 +0,0 @@
-# api/fastapi/dependencies.py
-from fastapi import Request, HTTPException
-
-def get_trenex_server(request: Request):
-    server = getattr(request.app.state, "_server", None)
-    if not server:
-        raise HTTPException(status_code=500, detail="TrenexServer not initialized")
-    return server
